3951568d1e7fcdd958069288c384d67c7dcbff0b
Yahya mallak <Yahya10mallak@gmail.com>
2021-11-05 03:10:16 +0100

    first flexbox commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlexBox</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <br><br><br>
+    <h1>
+        Flex 1
+    </h1>
+    <br><br><br>
+    <div class="container1">
+        <div class="c 1">1</div>
+        <div class="c 2">2</div>
+        <div class="c 3">3</div>
+        <div class="c 4">4</div>
+    </div>
+    <br><br><br>
+    <h1>
+        Flex 2
+    </h1>
+    <br><br><br>
+    <div class="container2">
+        <div class="c 5">5</div>
+        <div class="c 6">6</div>
+        <div class="c 7">7</div>
+        <div class="c 8">8</div>
+    </div>
+</body>
+
+</html>
